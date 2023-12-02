398adf2a631533569ab0afb8bc4dc6b8ec298dc9
Singireddy Bharath <singireddybharath99@gmail.com>
2023-12-02 13:40:40 +0530

    added server url

--- client/src/axios/instance.js
+++ client/src/axios/instance.js
@@ -1,19 +1,21 @@
 import axios from "axios";
-const isDev = true;
+const isDev = false;
 
-const url = isDev ? "http://localhost:8000" : "";
+const url = isDev
+  ? "http://localhost:8000"
+  : "https://password-manager-dy4g.onrender.com";
 
 const instance = axios.create({
-    baseURL: url,
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    },
-    withCredentials: true
+  baseURL: url,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 export const checkAuthenticated = () => instance.get("/authenticate");
-export const loginUser = (data) => instance.post("/login", data); 
+export const loginUser = (data) => instance.post("/login", data);
 export const logoutUser = () => instance.get("/logout");
 export const signupUser = (data) => instance.post("/register", data);
 export const saveNewPassword = (data) => instance.post("/addnewpassword", data);
