36c0c175571c3d9dbe6958907d8ccb52676a086c
Singireddy Bharath <singireddybharath99@gmail.com>
2023-12-02 14:44:34 +0530

    added sameSite

--- server/router/routing.js
+++ server/router/routing.js
@@ -60,6 +60,8 @@ router.post("/login", async (req, res) => {
       res.cookie("jwtoken", token, {
         expires: new Date(Date.now() + 2592000000),
         httpOnly: true,
+        secure: true,
+        sameSite: "None",
       });
 
       return res.status(200).json({ message: "User login successfully." });
